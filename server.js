874ef83f59c14e583c3cb37b4b64e3b0c3dcdaa9
techmannih <manishchaudharyttt@gmail.com>
2024-02-20 07:42:36 +0530

    add routes and cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const EventRouter = require("./routes/eventRoutes");
 
 // env config
 dotenv.config();
@@ -9,6 +11,26 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// Add a custom middleware to handle non-JSON requests
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    // Handle JSON parsing error
+    res.status(400).json({ error: "Invalid JSON" });
+  } else {
+    next();
+  }
+});
+
+// ... rest of your middleware and routes
+
+
+// Specify a base path for the event routes
+app.use("/api", EventRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
