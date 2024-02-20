8e87d9b5942b3920a80b85795ff6b22dcb8e0c37
techmannih <manishchaudharyttt@gmail.com>
2024-02-20 07:44:49 +0530

    add controllers

---
+++ controller/EventController.js
@@ -0,0 +1,60 @@
+const eventModel = require("../models/eventmodel"); // Updated import
+const path = require("path");
+const fs = require("fs");
+const express = require("express");
+const multer = require("multer");
+const { uploadOnCloudinary } = require("../config/cloudinary.service");
+
+// Create event
+const uploadMiddleware = multer({ dest: "uploads/" });
+
+// Create a new event
+exports.newEventController = async (req, res) => {
+  try {
+    const {
+      eventName,
+      description,
+      website,
+      instaHandle,
+      location,
+      date,
+      primaryDate,
+      lastDate,
+      primaryLocation,
+    } = req.body;
+
+    // Check if req.file is defined before accessing its path property
+    if (!req.file || !req.file.path) {
+      return res
+        .status(400)
+        .json({ message: "No file uploaded or file path is missing." });
+    }
+
+    const uploadResult = await uploadOnCloudinary(req.file.path);
+
+    // Assuming there is a typo in your code, and it should be "primaryLocation" instead of "location"
+    const newEvent = new eventModel({
+      eventName,
+      description,
+      website,
+      instaHandle,
+      date,
+      location, // This line might need to be updated to primaryLocation if that's the correct property
+      image: uploadResult.url,
+      primaryDate,
+      primaryLocation,
+      lastDate,
+    });
+
+    const savedEvent = await newEvent.save();
+
+    return res.status(201).json({
+      success: true,
+      message: "New event created",
+      event: savedEvent,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error in backend" });
+  }
+};
