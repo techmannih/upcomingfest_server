a8c7232e4f6bd2d2365d2adb855ddf9d33fa57f5
techmannih <manishchaudharyttt@gmail.com>
2024-02-20 20:46:31 +0530

    deploy timeout bug

--- config/db.js
+++ config/db.js
@@ -8,12 +8,13 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
+      connectTimeoutMS: 3000000000000000, // Adjusted to 30 seconds (30,000 milliseconds)
     });
     console.log("Connected to MongoDB Database " + mongoose.connection.host);
   } catch (error) {
-    console.error("MONGO Connect Error");
+    console.error("Error connecting to MongoDB:");
     console.error(error);
+    process.exit(1); // Exit the process if unable to connect to the database
   }
 };
 
