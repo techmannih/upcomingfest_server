ba1786bcc093339dbeb66a87f589ed8d577d4382
techmannih <manishchaudharyttt@gmail.com>
2024-02-20 07:43:09 +0530

    create schema for events

---
+++ models/eventmodel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const EventSchema = new mongoose.Schema(
+  {
+    eventName: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    website: {
+      type: String,
+    },
+    instaHandle: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    image: {
+      type: String,
+      // required: true,
+    },
+    primaryLocation: {
+      type: String,
+    },
+    date: {
+      type: String,
+    },
+    primaryDate: {
+      type: String,
+    },
+    lastDate: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const EventModel = mongoose.model("Event", EventSchema);
+module.exports = EventModel;
